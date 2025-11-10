95cef98e9b105207543b9d0d4f77728e05c896bc
yadavhimanshu1811 <yadavhimanshu1811@gmail.com>
2025-11-11 01:07:27 +0530

    Pushing remaining code

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,7 @@
 import { authOptions } from "@/lib/auth";
 import NextAuth from "next-auth";
 
-const handler = NextAuth(authOptions){
-    
-}
+const handler = NextAuth(authOptions)
+// {} TODO
 
 export{handler as GET, handler as POST};
